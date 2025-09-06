54c8b93e79761104f79c55c83f035e811b9223b1
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-09-06 10:34:47 -0500

    segundo ejempl0o con future builder

--- lib/pages/future_builder_page.dart
+++ lib/pages/future_builder_page.dart
@@ -15,6 +15,19 @@ class FutureBuilderPage extends StatelessWidget {
     throw Exception("Error al cargar datos");
   }
 
+  Future<List<Map<String, dynamic>>> getItems() {
+    return Future.delayed(Duration(seconds: 3), () {
+      return [
+        {"id": 1, "name": "zapatos"},
+        {"id": 2, "name": "Vestidos"},
+        {"id": 3, "name": "Zapatillas"},
+        {"id": 4, "name": "Polos"},
+        {"id": 5, "name": "Camisas"},
+        {"id": 6, "name": "Correas"},
+      ];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,40 +35,64 @@ class FutureBuilderPage extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            // SEGUNDO EJEMPLO DE FUTUREBUILDER
             FutureBuilder(
-              future: getData(),
+              future: getItems(),
               builder: (BuildContext context, AsyncSnapshot snapshot) {
-                print("------------------------------");
-                print("SNAPSHOT: $snapshot");
-                print("ESTADO DE CONEXIÓN: ${snapshot.connectionState}");
-                print("HAS ERROR: ${snapshot.hasError}");
-                print("ERROR: ${snapshot.error}");
-                print("HAS DATA: ${snapshot.hasData}");
-                print("DATA: ${snapshot.data}");
-                print("------------------------------");
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: SizedBox(
-                      height: 120,
-                      width: 120,
-                      child: CircularProgressIndicator(
-                        color: Colors.pink,
-                        strokeWidth: 20,
-                      ),
+                if (snapshot.hasData) {
+                  print(snapshot.data);
+                  List<Map<String, dynamic>> data = snapshot.data;
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(data[index]["name"]),
+                          leading: Text(data[index]["id"].toString()),
+                        );
+                      },
                     ),
                   );
-                } else if (snapshot.hasError) {
-                  return Text(
-                    "Error: ${snapshot.error}",
-                    style: TextStyle(color: Colors.red, fontSize: 40),
-                  );
-                } else if (snapshot.hasData) {
-                  return Text(snapshot.data, style: TextStyle(fontSize: 50));
-                } else {
-                  return Text("Sin datos", style: TextStyle(fontSize: 50));
                 }
+                return Center(child: CircularProgressIndicator());
               },
             ),
+
+            // PRIMER EJEMPLO CON FUTUREBUILDER
+            // FutureBuilder(
+            //   future: getData(),
+            //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+            //     print("------------------------------");
+            //     print("SNAPSHOT: $snapshot");
+            //     print("ESTADO DE CONEXIÓN: ${snapshot.connectionState}");
+            //     print("HAS ERROR: ${snapshot.hasError}");
+            //     print("ERROR: ${snapshot.error}");
+            //     print("HAS DATA: ${snapshot.hasData}");
+            //     print("DATA: ${snapshot.data}");
+            //     print("------------------------------");
+            //     if (snapshot.connectionState == ConnectionState.waiting) {
+            //       return Center(
+            //         child: SizedBox(
+            //           height: 120,
+            //           width: 120,
+            //           child: CircularProgressIndicator(
+            //             color: Colors.pink,
+            //             strokeWidth: 20,
+            //           ),
+            //         ),
+            //       );
+            //     } else if (snapshot.hasError) {
+            //       return Text(
+            //         "Error: ${snapshot.error}",
+            //         style: TextStyle(color: Colors.red, fontSize: 40),
+            //       );
+            //     } else if (snapshot.hasData) {
+            //       return Text(snapshot.data, style: TextStyle(fontSize: 50));
+            //     } else {
+            //       return Text("Sin datos", style: TextStyle(fontSize: 50));
+            //     }
+            //   },
+            // ),
           ],
         ),
       ),
